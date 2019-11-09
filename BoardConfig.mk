0418af1741645d745392a2f3c2519a671058cc92
Kirill <vsegda_online_007@bk.ru>
2019-11-10 00:39:51 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,29 @@
-# Device path
-LOCAL_PATH := device/umidigi/A5_Pro
+#
+# Copyright (C) 2019 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Basics & Paths
+BOARD_VENDOR := umidigi
+DEVICE_PATH := device/umidigi/A5_Pro
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6763
+TARGET_NO_BOOTLOADER := true
+
+# Board Platform
+TARGET_BOARD_PLATFORM := mt6763
 
 # Architecture
 TARGET_ARCH := arm64
@@ -12,35 +36,27 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := armv8-a
-TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
-TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
-TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
 TARGET_USES_64_BIT_BINDER := true
 
-BOARD_USES_MTK_HARDWARE := true
-
-# Platform
-TARGET_BOARD_PLATFORM := mt6763
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := mt6763
-
-# Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+# Kernel Rules
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_HEADER_SIZE := 1648
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_HASH_TYPE := sha1
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_RAMDISK_OFFSET := 0x14f88000
 BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x14f88000
 BOARD_SECOND_OFFSET := 0x00e88000
 BOARD_TAGS_OFFSET := 0x13f88000
-BOARD_RECOVERY_DTBO_OFFSET := 16803840
-BOARD_BOOTIMG_HEADER_VERSION := 1
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_RECOVERY_DTBO_SIZE := 42169
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET) --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# DTBO Rules
+BOARD_RECOVERY_DTBO_SIZE := 42169
+BOARD_RECOVERY_DTBO_OFFSET := 16803840
+BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := $(LOCAL_PATH)/prebuilt/recovery_dtbo.img
 
 #system.prop
@@ -49,7 +65,6 @@ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 #Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 2793406464
-BOARD_FLASH_BLOCK_SIZE := 0
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -60,6 +75,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072 # BOARD_KERNEL_PAGESIZE * 64
 
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Workaround for error copying vendor files to recovery ramdisk
@@ -71,14 +87,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-#BOARD_HAS_NO_MISC_PARTITION := true
-#BOARD_SUPPRESS_EMMC_WIPE := true
-#BOARD_RECOVERY_SWIPE := true
-#BOARD_USES_MMCUTILS := true
 BOARD_AVB_ENABLE := true
-#BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/battery"
